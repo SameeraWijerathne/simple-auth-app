06819afb40bf748de42ad5dd5fee5def21256dc4
Sameera <sameerawncd@gmail.com>
2023-08-22 23:26:39 +0530

    Implemented cors policy

--- auth-back-end/src/main/java/lk/wsrp/sameera/auth/WebAppConfig.java
+++ auth-back-end/src/main/java/lk/wsrp/sameera/auth/WebAppConfig.java
@@ -2,10 +2,18 @@ package lk.wsrp.sameera.auth;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true);
+    }
 }
