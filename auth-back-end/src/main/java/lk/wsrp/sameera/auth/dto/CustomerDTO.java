7e1a05dc395193a3058933ab5374d0347f33787f
Sameera <sameerawncd@gmail.com>
2023-08-22 15:00:06 +0530

    Created CustomerDTO

---
+++ auth-back-end/src/main/java/lk/wsrp/sameera/auth/dto/CustomerDTO.java
@@ -0,0 +1,16 @@
+package lk.wsrp.sameera.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerDTO {
+    private Integer id;
+    private String name;
+    private String address;
+}
