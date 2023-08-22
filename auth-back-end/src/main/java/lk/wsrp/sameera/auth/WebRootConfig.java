e4cf9fda244cd789d09f240240a164924a432bf3
Sameera <sameerawncd@gmail.com>
2023-08-22 15:56:35 +0530

    Complete WebRootConfig class

--- auth-back-end/src/main/java/lk/wsrp/sameera/auth/WebRootConfig.java
+++ auth-back-end/src/main/java/lk/wsrp/sameera/auth/WebRootConfig.java
@@ -1,7 +1,20 @@
 package lk.wsrp.sameera.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 public class WebRootConfig {
+    @RequestScope
+    @Bean
+    public Connection connection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/sameera_auth?createDatabaseIfNotExist=true",
+                "root", "mysql");
+    }
 }
