b17e751479829fa9c9b3544c2c3fe6fde5c6e414
Sameera <sameerawncd@gmail.com>
2023-08-23 23:55:29 +0530

    Finalized login.js

---
+++ auth-front-end/src/js/login.js
@@ -0,0 +1,35 @@
+import {API_BASE_URL} from "./main.js";
+
+const frmLogin = $("#frm-login");
+const username = $("#username");
+const password = $("#password");
+
+frmLogin.on('click', (evt) => {
+    evt.preventDefault();
+
+    const jqxhr = $.ajax(`${API_BASE_URL}/auth/login`, {
+        method: 'POST',
+        contentType: 'application/json',
+        xhrFields: {
+            withCredentials: true
+        },
+        data: JSON.stringify({
+            username: username.val().trim(),
+            password: password.val().trim()
+        })
+    });
+
+    jqxhr.done(() => {
+        localStorage.setItem("username", username.val().trim());
+        location.replace('index.html');
+    });
+
+    jqxhr.fail(() => {
+        if (jqxhr.status === 401) {
+            alert("Invalid login credentials");
+        } else {
+            alert("Something went wrong, please try again!");
+        }
+        username.trigger('focus');
+    });
+});
